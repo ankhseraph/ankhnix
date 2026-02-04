27947831e41e4c93bb9a4522104efa8ef1a98081
ankhseraph <git@ankhseraph.com>
2026-02-04 20:51:34 +0200

    Rebuild: 456 (2026-02-03 23:18:59)

---
+++ system/network/storage.nix
@@ -0,0 +1,21 @@
+{ ... }:
+
+{
+  # nas mount
+  fileSystems."/mnt/nas" = {
+    device = "//192.168.0.123/Mihaita";
+    fsType = "cifs";
+
+    options = [
+      "username=mihaita"
+      "password=azuredragon"  # todo: credentials file
+      "uid=ankhangel"
+      "gid=users"
+      "iocharset=utf8"
+      "vers=3.1.1"
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.idle-timeout=60"
+    ];
+  };
+}
